113e123508dcf6b211b31a444e15d90d71047a41
Albermagen <albertomagenciomiguel@gmail.com>
2025-09-25 12:15:31 +0200

    Primeros cambios

--- p1-contador/fase1/script.js
+++ p1-contador/fase1/script.js
@@ -3,21 +3,22 @@ const spanContador = document.getElementById("contador");
 const btnMas = document.getElementById("btn-mas");
 const btnMenos = document.getElementById("btn-menos");
 
-function actualizarContador() {
-  spanContador.textContent = contador;
-
+function actualizarContador() {  
+  spanContador.textContent = parseFloat(contador).toFixed(1);
   // Efecto visual para marcar el cambio
   spanContador.classList.add("changed");
   setTimeout(() => spanContador.classList.remove("changed"), 200);
 }
 
 btnMas.addEventListener("click", () => {
-  contador++;
+  contador+=0.1;
+  if (contador>=10) contador=10;
   actualizarContador();
 });
 
 btnMenos.addEventListener("click", () => {
-  contador--;
+  contador-=0.1;
+  if (contador<=0) contador=0;
   actualizarContador();
 });
 
