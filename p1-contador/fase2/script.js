c400cd74ac13c1c6513c9f1829356f2eab12d90d
RobertoCH-LY <tu-email@example.com>
2025-09-26 10:46:32 +0200

    Primer commit con archivos de DI20252026

--- p1-contador/fase2/script.js
+++ p1-contador/fase2/script.js
@@ -107,6 +107,9 @@ lista.addEventListener("click", (ev) => {
   if (btn.classList.contains("btn-mas")) valor += 0.1;
   if (btn.classList.contains("btn-menos")) valor -= 0.1;
 
+// Limitar el valor máximo a 10
+  if (valor > 10) valor = 10;
+
   estado.set(nombre, valor);
   span.dataset.valor = String(valor);
   span.textContent = valor;
